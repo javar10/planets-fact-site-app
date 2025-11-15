15be9f2e85fef24dd5f533e0857e525e9ff7e4a0
Jessica Vargas <139159958+javar10@users.noreply.github.com>
2025-11-14 22:22:26 -0500

    planet page nav

--- App.js
+++ App.js
@@ -43,6 +43,7 @@ export default function App() {
             <PlanetsMenu
               selectedPlanet={selectedPlanet}
               setSelectedPlanet={setSelectedPlanet}
+              setIsMobileMenuOpen={setIsMobileMenuOpen}
             />
           </View>
         }
